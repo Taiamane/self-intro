f7d41c4b86952e1af43272327e01957a74365dd8
Taiamane <fujiura-taishu749@g.ecc.u-tokyo.ac.jp>
2025-07-16 14:43:08 +0900

    ヘッダーの雛形完了

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './header.css'
+import React, { useState } from 'react';
+import { Link,BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="App">
+      <BrowserRouter>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p className='page_title'>
+          Amane-Portal
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+          <Link to="/" onClick={() => setIsMenuOpen(false)}>Top</Link>
+          <Link to="/howtouse" onClick={() => setIsMenuOpen(false)}>PortForio</Link>
+          <Link to="/pricing" onClick={() => setIsMenuOpen(false)}>Blogs</Link>
+          <Link to="/contact" onClick={() => setIsMenuOpen(false)}>Another</Link>
+        </div>
+
+        <button className="hamburger-menu" onClick={toggleMenu}>
+        {/* ハンバーガーの三本線 */}
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+
       </header>
+
+
+      
+        <Routes>
+        
+        </Routes>
+      </BrowserRouter>
     </div>
+
+    
   );
 }
 
