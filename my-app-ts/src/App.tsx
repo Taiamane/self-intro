d13459a2501d2744908f6a685af7e0497453987c
Taiamane <fujiura-taishu749@g.ecc.u-tokyo.ac.jp>
2025-07-18 17:30:18 +0900

    雛形作成

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css';
 import './header.css'
+import './footer.css'
 import React, { useState } from 'react';
 import { Link,BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import profilePic from'./picture/testpic.jpg'
 
 function App() {
 
@@ -32,9 +33,28 @@ function App() {
           <span></span>
           <span></span>
         </button>
-
-
       </header>
+
+      <body>
+        あああああああああ
+      </body>
+
+      <footer className='App-footer'>
+        <div className='picture-and-desc'>
+          <img
+          src={profilePic}
+          alt="プロフィール画像"
+          className='footer-content profileImage'
+            />
+          <div className='footer-content description'>
+            <p>自己紹介文だよ</p>
+            <p>自己紹介文二つ目だよ</p>
+            <p>自己紹介文二つ目だよ</p>
+            <p>自己紹介文二つ目だよ</p>
+          </div>
+        </div>
+      <div className="copyright"> 2024 Amane</div>
+      </footer>
 
 
       
